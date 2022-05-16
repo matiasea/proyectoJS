5aec4851293cb405a0626ce87eac4f3521f01fb7
matiasea <mati.almada02@gmail.com>
2022-05-16 18:10:50 -0300

    "actualizacion"

--- js/verCarrito.js
+++ js/verCarrito.js
@@ -4,25 +4,70 @@
 
 const productosSeleccionados = JSON.parse(localStorage.getItem("carrito")) || [];
 productosSeleccionados.length === 0 && alert("El Carrito está vacío") 
+let cardGroup2 = document.getElementsByClassName("table")
+productosCarrito();
 
 
+
+function productosCarrito(){
     for (let i = 0; i < productosSeleccionados.length; i++) {
     const element = productosSeleccionados[i];
     const {articulo, marca, precio, img} = element;
-    const cardGroup2 = document.getElementsByClassName("table")
     const dom = `<tbody>
           <tr>
             <th scope="row"><img src="${img} " alt="" width="80px"</th>
             <td>${articulo} </td>
             <td>${marca}</td>
             <td>$${precio} </td>
-            <td> <button class="btn btn-outline-secondary botonEliminar" id="botonEliminar"><img src="./img/LUPA.png" width = "20px" alt=""></button></td>
+            <td> <button class="btn btn-outline-secondary" id="botonEliminar"><img src="./img/LUPA.png" width = "20px" alt=""></button></td>
     </tbody>`  
     cardGroup2[0].innerHTML += dom;
     
 }
+}
+
+// -------------ELIMINAR DEL CARRITO --------------
+
+    
+const eliminarDelCarrito1 = (prodId) => {
+    const productoEncontrado = productosSeleccionados.find(prod  => prod.id === prodId);
+    const indice = productosSeleccionados.indexOf(productoEncontrado);
+    productosSeleccionados.splice(indice, 1);
+    actualizarCarrito();
+    console.log(productosSeleccionados)
+    
+
+    /* let btnEliminar = document.getElementById("botonEliminar");
+    btnEliminar.onclick = () => {
+    eliminarDelCarrito1()
+     */
+    
+    }
+
+const botonEliminar = document.getElementById("botonEliminar");
+ for (let i = 0; i < botonEliminar.length; i++) {
+    const element = botonEliminar[i]; 
+    botonEliminar.addEventListener("click", eliminarDelCarrito1)
+}
+   
+
+console.log(productosSeleccionados)
+console.log(carrito)
 
 
+
+//-------------------------------------------------------------------
+//--------------------------ACTUALIZAR CARRITO --------------------
+
+const actualizarCarrito= () => {
+    productosSeleccionados = {};
+    productosCarrito()
+}
+
+
+
+
+//---------------------------------------------------
 //--------------------CALCULAR TOTAL-----------------
 
 for (let i = 0; i < productosSeleccionados.length; i++) {
@@ -49,25 +94,3 @@ const tablaTotal = document.getElementsByClassName("table-2");
 
 
 //actualizarCarrito();
-//--------------------------ACTUALIZAR CARRITO --------------------
-
-const actualizarCarritos= () => {
-    cardGroup2[0].innerHTML = "";
-    
-    for (let i = 0; i < productosSeleccionados.length; i++) {
-    const element = productosSeleccionados[i];
-    const {articulo, marca, precio, img} = element;
-    const cardGroup2 = document.getElementsByClassName("table")
-    const dom = `<tbody>
-          <tr>
-            <th scope="row"><img src="${img} " alt="" width="80px"</th>
-            <td>${articulo} </td>
-            <td>${marca}</td>
-            <td>$${precio} </td>
-            <td> <button class="btn btn-outline-secondary botonEliminar" id="botonEliminar"><img src="./img/LUPA.png" width = "20px" alt=""></button></td>
-    </tbody>
-    
-    `  
-    cardGroup2[0].innerHTML += dom;
-}
-}
